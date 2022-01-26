b3332ebb514727735eeee209fa94a6b1f48c1535
amitkpradhan <amitpradhan.devops@gmail.com>
2022-01-26 18:25:54 +0530

    initial commit

---
+++ MyWindowsService/FileIO.vb
@@ -0,0 +1,17 @@
+﻿Public Class FileIO
+    Public Shared Sub WriteToFile(strToWrite As String)
+        Dim stream As IO.StreamWriter = Nothing
+        Try
+            stream = New IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\myFile.txt", True)
+
+            stream.Write(strToWrite)
+            stream.Flush()
+            stream.Close()
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Class
---
+++ MyWindowsService/Service1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Service1
+
+    Dim tmr As Timers.Timer
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' Add code here to start your service. This method should set things
+        ' in motion so your service can do its work.
+        tmr = New Timers.Timer()
+        tmr.Interval = 1000
+        AddHandler tmr.Elapsed, AddressOf MyTickHandler
+        tmr.Enabled = True
+        FileIO.WriteToFile("Service is Started!" + vbNewLine)
+    End Sub
+
+    Protected Overrides Sub OnStop()
+
+        tmr.Enabled = False
+        FileIO.WriteToFile("Service is Stopped!" + vbNewLine)
+
+    End Sub
+
+    Private Sub MyTickHandler(obj As Object, e As EventArgs)
+
+        FileIO.WriteToFile("1 sec is over!" + vbNewLine)
+    End Sub
+
+
+
+End Class
